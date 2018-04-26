c582aeee1a5ff532713320283280d7ecef298b3c
hasilovasarana <saranakhh@code.edu.az>
2018-04-27 00:55:49 +0400

    created search system

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace learn_json
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
